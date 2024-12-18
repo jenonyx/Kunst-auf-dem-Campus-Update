f5f267f4bc96d36ee02a5b1698d6dd4c61cfa9e1
jenonyx <jenni.altmann11@web.de>
2024-12-18 17:08:36 +0100

    added Screenshots of visualizations

--- public/js/app.js
+++ public/js/app.js
@@ -31,14 +31,32 @@
   
   // Description display div
   const descriptionDisplay = document.getElementById('description-display');
+  const titleText = document.getElementById('title-text');
   const descriptionText = document.getElementById('description-text');
 
   // Add click event listener to each card
   cards.forEach(card => {
     card.addEventListener('click', () => {
+      //TODO: Give each card an ID
+      //TODO: EXtract JSON file description, id, Title (?)
+      //TODO: change static description to one for each card ID corresponding to the same id in JSON file
+          // Check if the div is already visible
+      const cardId = card.dataset.cardId; // get the clicked card's ID
+      const isVisible = !descriptionDisplay.classList.contains('hidden');
+          // If the clicked card's content is already displayed, hide it
+    if (isVisible && descriptionDisplay.dataset.activeCard === card.dataset.cardId) {
+      descriptionDisplay.classList.add('hidden'); // Hide the description display
+      descriptionDisplay.dataset.activeCard = ""; // Clear active card reference
+    } else {
       //const description = card.getAttribute('data-description');
+      const title = "Fotodruck";
       const description = "Dieser Fotodruck befindet sich neben den Treppen, die vom Forumsplatz hoch zum Eingang der Universitätsbibliothek führen. Er zeigt die Galaxie NGC 2336 und wurde von der Fakultät für Physik und Astronomie angebracht.";
       descriptionText.textContent = description;
+      titleText.textContent = title;
+
+      descriptionDisplay.classList.remove('hidden'); // Show the description display
+      descriptionDisplay.dataset.activeCard = card.dataset.cardId; // Set active card reference
+    }
       
     });
   });
