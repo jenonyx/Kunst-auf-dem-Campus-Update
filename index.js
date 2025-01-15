30e215c3d617ae149829e466419368fc6a1c04bf
jenonyx <jenni.altmann11@web.de>
2025-01-15 01:42:59 +0100

    config.js datei hinzugefügt, relative pfade möglich

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const engine = require('ejs-mate');
-
+const config = require('./config/config.js'); // Update the routes depending on the location of the project
 // use ejs-locals for all ejs templates:
 app.engine('ejs', engine);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+var indexRouter = express.Router();
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(config.baseUrl, indexRouter); // Mount the app on the base URL
 
 // Define the navigation links:
 const navLinks = {
@@ -22,37 +24,37 @@ const navLinks = {
   
 
 // Routes:
-app.get('/', (req, res) => {
+indexRouter.get('/', (req, res) => {
     let activePage = 'home';
     res.render('home', { navLinks, activePage }); // Pass the navigation links to the EJS template
 });
 
-app.get('/map', (req, res) => {
+indexRouter.get('/map', (req, res) => {
     let activePage = 'map';
     res.render('map', { navLinks, activePage});
 });
 
-app.get('/visualizations', (req, res) => {
+indexRouter.get('/visualizations', (req, res) => {
     let activePage = 'visualizations';
     res.render('visualizations', { navLinks, activePage });
 });
 
-app.get('/about', (req, res) => {
+indexRouter.get('/about', (req, res) => {
     let activePage = 'about';
     res.render('about', { navLinks, activePage });
 });
 
-app.get('/quellen', (req, res) => {
+indexRouter.get('/quellen', (req, res) => {
     let activePage = 'quellen';
     res.render('quellen', { navLinks, activePage });
 });
 
-app.get('/stories/:path', (req, res) => {
+indexRouter.get('/stories/:path', (req, res) => {
     const { path } = req.params;    // Extract the path parameter
     res.render(`stories/${path}`); // Dynamically render the EJS file
   });
   
 
-app.listen(3000, () => { // Start the server on port 3000
-    console.log("Listening on port 3000");
+app.listen(3001, () => { // Start the server on port 3001
+    console.log("Listening on port 3001");
 })
